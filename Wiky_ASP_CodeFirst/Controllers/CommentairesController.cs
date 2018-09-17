fa9b30c060d4d25bcbf9430eb685fbf67ab3de66
GuillaumeP <guillaume.pani@gmail.com>
2018-09-17 17:06:36 +0200

    Add controllers

---
+++ Wiky_ASP_CodeFirst/Controllers/CommentairesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wiky_ASP_CodeFirst.BDD_CF;
+using Wiky_ASP_CodeFirst.Models;
+
+namespace Wiky_ASP_CodeFirst.Controllers
+{
+    public class CommentairesController : Controller
+    {
+        private Wiky_CF db = new Wiky_CF();
+
+        // GET: Commentaires
+        public ActionResult Index()
+        {
+            var commentaires = db.Commentaires.Include(c => c.Article);
+            return View(commentaires.ToList());
+        }
+
+        // GET: Commentaires/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commentaire commentaire = db.Commentaires.Find(id);
+            if (commentaire == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commentaire);
+        }
+
+        // GET: Commentaires/Create
+        public ActionResult Create()
+        {
+            ViewBag.Com_Id_Article = new SelectList(db.Articles, "Id_Article", "Theme");
+            return View();
+        }
+
+        // POST: Commentaires/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Commentaire,Auteur,DateComentaire,Contenu,Com_Id_Article")] Commentaire commentaire)
+        {
+            if (ModelState.IsValid)
+            {
+                commentaire.DateComentaire = DateTime.Now;
+                db.Commentaires.Add(commentaire);
+                db.SaveChanges();
+                return RedirectToAction("Details", "Articles", new {id = commentaire.Com_Id_Article });
+            }
+
+            ViewBag.Com_Id_Article = new SelectList(db.Articles, "Id_Article", "Theme", commentaire.Com_Id_Article);
+            return View(commentaire);
+        }
+
+        // GET: Commentaires/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commentaire commentaire = db.Commentaires.Find(id);
+            if (commentaire == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Com_Id_Article = new SelectList(db.Articles, "Id_Article", "Theme", commentaire.Com_Id_Article);
+            return View(commentaire);
+        }
+
+        // POST: Commentaires/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Commentaire,Auteur,DateComentaire,Contenu,Com_Id_Article")] Commentaire commentaire)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(commentaire).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Com_Id_Article = new SelectList(db.Articles, "Id_Article", "Theme", commentaire.Com_Id_Article);
+            return RedirectToAction("Details", "Articles", new {id = commentaire.Com_Id_Article });
+        }
+
+        // GET: Commentaires/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commentaire commentaire = db.Commentaires.Find(id);
+            if (commentaire == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commentaire);
+        }
+
+        // POST: Commentaires/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Commentaire commentaire = db.Commentaires.Find(id);
+            db.Commentaires.Remove(commentaire);
+            db.SaveChanges();
+            return RedirectToAction("Details", "Articles", new {id = commentaire.Com_Id_Article });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Wiky_ASP_CodeFirst/Controllers/HomeController.cs
+++ Wiky_ASP_CodeFirst/Controllers/HomeController.cs
@@ -30,80 +30,80 @@ namespace Wiky_ASP_CodeFirst.Controllers
 
             return View();
         }
+        ////[HttpGet]
+        ////public ActionResult Test()
+        ////{
+        ////    ViewBag.notif = "";
+        ////    return View();
+        ////}
+
+        ////[HttpPost]
+        ////public ActionResult Test(Article model)
+        ////{
+        ////    ViewBag.notif = $"{model.Theme}";
+        ////    model.DateCreation = DateTime.Now;
+        ////    DBLocator.DbContext.Articles.Add(model);
+        ////    DBLocator.DbContext.SaveChanges();
+        ////    model = new Article();
+        ////    return View();
+        ////}
+
         //[HttpGet]
-        //public ActionResult Test()
+        //public ActionResult AjoutArticle()
         //{
-        //    ViewBag.notif = "";
         //    return View();
         //}
 
         //[HttpPost]
-        //public ActionResult Test(Article model)
+        //public ActionResult AjoutArticle(Article a)
         //{
-        //    ViewBag.notif = $"{model.Theme}";
-        //    model.DateCreation = DateTime.Now;
-        //    DBLocator.DbContext.Articles.Add(model);
+        //    a.DateCreation = DateTime.Now;
+        //    DBLocator.DbContext.Articles.Add(a);
         //    DBLocator.DbContext.SaveChanges();
-        //    model = new Article();
         //    return View();
         //}
 
-        [HttpGet]
-        public ActionResult AjoutArticle()
-        {
-            return View();
-        }
 
-        [HttpPost]
-        public ActionResult AjoutArticle(Article a)
-        {
-            a.DateCreation = DateTime.Now;
-            DBLocator.DbContext.Articles.Add(a);
-            DBLocator.DbContext.SaveChanges();
-            return View();
-        }
+        //public ActionResult ListeArticles()
+        //{
 
+        //    var model = DBLocator.DbContext.Articles.ToList();
+        //    return View(model);
+        //}
 
-        public ActionResult ListeArticles()
-        {
+        //[HttpGet]
+        //public ActionResult DetailArticle(int id)
+        //{
+        //    Article model = DBLocator.DbContext.Articles.Find(id);
+        //    return View(model);
+        //}
+        //[HttpGet]
+        //public ActionResult EditArticle(int id)
+        //{
+        //    Article model = DBLocator.DbContext.Articles.Find(id);
+        //    return View(model);
+        //}
 
-            var model = DBLocator.DbContext.Articles.ToList();
-            return View(model);
-        }
+        //[HttpPost]
+        //public ActionResult EditArticle(Article model)
+        //{
+        //    Article model1 = DBLocator.DbContext.Articles.Find(model.Id_Article);
+        //    model1.Contenu = model.Contenu;
+        //    model1.Theme = model.Theme;
+        //    model1.Auteur = model.Auteur;
+        //    model1.DateCreation = model.DateCreation;
+        //    DBLocator.DbContext.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
-        [HttpGet]
-        public ActionResult DetailArticle(int id)
-        {
-            Article model = DBLocator.DbContext.Articles.Find(id);
-            return View(model);
-        }
-        [HttpGet]
-        public ActionResult EditArticle(int id)
-        {
-            Article model = DBLocator.DbContext.Articles.Find(id);
-            return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult EditArticle(Article model)
-        {
-            Article model1 = DBLocator.DbContext.Articles.Find(model.Id_Article);
-            model1.Contenu = model.Contenu;
-            model1.Theme = model.Theme;
-            model1.Auteur = model.Auteur;
-            model1.DateCreation = model.DateCreation;
-            DBLocator.DbContext.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        public ActionResult AddCom(Commentaire com)
-        {
-            com.DateComentaire = DateTime.Now;
-            DBLocator.DbContext.Commentaires.Add(com);
-            DBLocator.DbContext.SaveChanges();
-            return RedirectToAction("DetailArticle", new { id = com.Com_Id_Article });
-        }
+        //[HttpPost]
+        //public ActionResult AddCom(Commentaire com)
+        //{
+        //    com.DateComentaire = DateTime.Now;
+        //    DBLocator.DbContext.Commentaires.Add(com);
+        //    DBLocator.DbContext.SaveChanges();
+        //    return RedirectToAction("DetailArticle", new { id = com.Com_Id_Article });
+        //}
 
 
 
@@ -112,37 +112,37 @@ namespace Wiky_ASP_CodeFirst.Controllers
 
 
 
-        [HttpGet]
-        public ActionResult EditCom(int id)
-        {
-            var com = DBLocator.DbContext.Commentaires.Find(id);
-            return View(com);
-        }
-        [HttpPost]
-        public ActionResult EditCom(Commentaire com)
-        {
-            var com1 = DBLocator.DbContext.Commentaires.Find(com.Id_Commentaire);
-            com1.Auteur = com.Auteur;
-            com1.Contenu = com.Contenu;
-            DBLocator.DbContext.SaveChanges();
-            return RedirectToAction("DetailArticle", new { id = com.Com_Id_Article });
-        }
+        //[HttpGet]
+        //public ActionResult EditCom(int id)
+        //{
+        //    var com = DBLocator.DbContext.Commentaires.Find(id);
+        //    return View(com);
+        //}
+        //[HttpPost]
+        //public ActionResult EditCom(Commentaire com)
+        //{
+        //    var com1 = DBLocator.DbContext.Commentaires.Find(com.Id_Commentaire);
+        //    com1.Auteur = com.Auteur;
+        //    com1.Contenu = com.Contenu;
+        //    DBLocator.DbContext.SaveChanges();
+        //    return RedirectToAction("DetailArticle", new { id = com.Com_Id_Article });
+        //}
 
-        public ActionResult DeleteCom(int id)
-        {
-            var com = DBLocator.DbContext.Commentaires.Find(id);
-            DBLocator.DbContext.Commentaires.Remove(com);
-            DBLocator.DbContext.SaveChanges();
-            return RedirectToAction("DetailArticle", new { id = com.Com_Id_Article });
-        }
-        public ActionResult DeleteArticle(int id)
-        {
-            var article = DBLocator.DbContext.Articles.Find(id);
-            DBLocator.DbContext.Articles.Remove(article);
-            DBLocator.DbContext.SaveChanges();
-            return RedirectToAction("ListeArticles");
+        //public ActionResult DeleteCom(int id)
+        //{
+        //    var com = DBLocator.DbContext.Commentaires.Find(id);
+        //    DBLocator.DbContext.Commentaires.Remove(com);
+        //    DBLocator.DbContext.SaveChanges();
+        //    return RedirectToAction("DetailArticle", new { id = com.Com_Id_Article });
+        //}
+        //public ActionResult DeleteArticle(int id)
+        //{
+        //    var article = DBLocator.DbContext.Articles.Find(id);
+        //    DBLocator.DbContext.Articles.Remove(article);
+        //    DBLocator.DbContext.SaveChanges();
+        //    return RedirectToAction("ListeArticles");
 
-        }
+        //}
 
 
     }
